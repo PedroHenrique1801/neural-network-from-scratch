9224e8342868cdb8982c740e0cf38b0b9b86cd97
Pedro Henrique <pedrohsantos1801@gmail.com>
2026-07-23 10:21:53 -0300

    Clean up comments in the feedforward function
    Removed commented-out code for clarity and readability.

--- network.py
+++ network.py
@@ -56,7 +56,6 @@ class OurNeuralNetwork:
 
     for epoch in range(epochs):
       for x, y_true in zip(data, all_y_trues):
-        # --- Do a feedforward (we'll need these values later)
         sum_h1 = self.w1 * x[0] + self.w2 * x[1] + self.b1
         h1 = sigmoid(sum_h1)
 
@@ -66,36 +65,25 @@ class OurNeuralNetwork:
         sum_o1 = self.w5 * h1 + self.w6 * h2 + self.b3
         o1 = sigmoid(sum_o1)
         y_pred = o1
-
-        # --- Calculate partial derivatives.
-        # --- Naming: d_L_d_w1 represents "partial L / partial w1"
         d_L_d_ypred = -2 * (y_true - y_pred)
-
-        # Neuron o1
         d_ypred_d_w5 = h1 * deriv_sigmoid(sum_o1)
         d_ypred_d_w6 = h2 * deriv_sigmoid(sum_o1)
         d_ypred_d_b3 = deriv_sigmoid(sum_o1)
 
         d_ypred_d_h1 = self.w5 * deriv_sigmoid(sum_o1)
         d_ypred_d_h2 = self.w6 * deriv_sigmoid(sum_o1)
-
-        # Neuron h1
+        
         d_h1_d_w1 = x[0] * deriv_sigmoid(sum_h1)
         d_h1_d_w2 = x[1] * deriv_sigmoid(sum_h1)
         d_h1_d_b1 = deriv_sigmoid(sum_h1)
-
-        # Neuron h2
         d_h2_d_w3 = x[0] * deriv_sigmoid(sum_h2)
         d_h2_d_w4 = x[1] * deriv_sigmoid(sum_h2)
         d_h2_d_b2 = deriv_sigmoid(sum_h2)
 
-        # --- Update weights and biases
-        # Neuron h1
         self.w1 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_w1
         self.w2 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_w2
         self.b1 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_b1
 
-        # Neuron h2
         self.w3 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_w3
         self.w4 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_w4
         self.b2 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_b2
